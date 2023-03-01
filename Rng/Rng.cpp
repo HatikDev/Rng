5d1118b355b476bbf06ef14832cb0e9af4d23664
Sergey Doroshin <sergeybezzabotnovv@gmail.com>
2023-03-01 18:17:29 +0300

    Fix bug with launching template functions

--- Rng/Rng.cpp
+++ Rng/Rng.cpp
@@ -1,9 +1,65 @@
 ﻿#include <iostream>
 #include <vector>
 
+#include "Analysis.h"
 #include "Constants.h"
 #include "Generator.h"
 #include "Levenshtein.h"
+
+template <size_t length>
+void launchAnalysisTestAllSetsAuto1(const std::string& inputFileName, const std::string& outputFileName)
+{
+    using result_type = std::map<std::string, size_t, CompType>;
+    size_t count = pow(2, length);
+
+    std::vector<uint8_t> batch;
+    std::vector<result_type> allResults;
+    for (size_t j = 0; j < count; ++j)
+    {
+        result_type result(comparator);
+
+        const std::string pattern = std::bitset<3/*length*/>(j).to_string();
+
+        std::string str;
+        for (size_t i = 0; i < kLength / kBatchLength; ++i)
+        {
+            readBatch(inputFileName, i * kBatchLength, batch);
+            analysisTest(batch, result, pattern, str);
+        }
+
+        auto begin = result.begin();
+        auto end = result.begin();
+        std::advance(end, result.size() > kResultCount ? kResultCount : result.size());
+        allResults.push_back(result_type(begin, end, comparator));
+    }
+
+    std::vector<result_type::iterator> iterators;
+    std::vector<result_type::iterator> endIterators;
+    iterators.reserve(allResults.size());
+    endIterators.reserve(allResults.size());
+    for (auto& res : allResults)
+    {
+        iterators.emplace_back(res.begin());
+        endIterators.emplace_back(res.end());
+    }
+
+    std::ofstream file(outputFileName, std::ios::app);
+    for (size_t i = 0; i < kResultCount; ++i)
+    {
+        for (size_t i = 0; i < iterators.size(); ++i)
+        {
+            auto& iter = iterators[i];
+            if (iter == endIterators[i])
+                continue;
+
+            file << "'" << (*iter).first << "'" << "," << (*iter).second << ",";
+            ++iter;
+        }
+
+        file << std::endl;
+    }
+    file.close();
+}
 
 int main()
 {
@@ -27,30 +83,30 @@ int main()
     //auto seq2 = getRandomBlockFromGenerator(SupportedGenerators::AESRNG, 1024);
     //levenshteinTest2(seq1, seq2);
 
-    for (size_t i = 0; i < 10; ++i)
-        getAllLevenshteinTestsResult();
+    //for (size_t i = 0; i < 10; ++i)
+    //    getAllLevenshteinTestsResult();
 
-    /*for (size_t j = 7; j <= 10; ++j)
-    {
-        std::string folderNumber = std::to_string(j);
-        //std::vector<std::string> inputFileNames = { /*"1/inputSystem.bin", folderNumber + "/inputLFSRSimple.bin", folderNumber + "/inputKnuthan.bin"/*, folderNumber + "/inputAESRNG.bin", folderNumber + "/inputMersenne.bin"*/// };
-        //std::vector<std::string> outpuFileNames = { /*"1/outputSystem.csv",*/ folderNumber + "/outputLFSRSimple.csv", folderNumber + "/outputKnuthan.csv"/*, folderNumber + "/outputAESRNG.csv", folderNumber + "/outputMersenne.csv"*/ };
-        //std::vector<SupportedGenerators> generators = { /*SupportedGenerators::SystemGenerator,*/ SupportedGenerators::Linear,
-        //                                                /*SupportedGenerators::X917RNG,*/ /*SupportedGenerators::AESRNG,*/ /*SupportedGenerators::MersenneTwister*/ };
-        //std::vector<SupportedGenerators> generators = { SupportedGenerators::Knuthan, SupportedGenerators::LFSRSimple };
-        //std::vector<std::string> inputFileNames = { "inputKnuthan.bin", "inputLFSRSimple.bin" };
-        //for (size_t i = 0; i < inputFileNames.size(); ++i)
-        //{
-        //    generateFromGenerator(inputFileNames[i], generators[i], kLength);
-        //}
+    //for (size_t j = 7; j <= 10; ++j)
+    //{
+    //    std::string folderNumber = std::to_string(j);
+    //    std::vector<std::string> inputFileNames = { /*"1/inputSystem.bin", folderNumber + "/inputLFSRSimple.bin", folderNumber + "/inputKnuthan.bin"/*, folderNumber + "/inputAESRNG.bin", folderNumber + "/inputMersenne.bin"*/// };
+    //    std::vector<std::string> outpuFileNames = { /*"1/outputSystem.csv",*/ folderNumber + "/outputLFSRSimple.csv", folderNumber + "/outputKnuthan.csv"/*, folderNumber + "/outputAESRNG.csv", folderNumber + "/outputMersenne.csv"*/ };
+    //    std::vector<SupportedGenerators> generators = { /*SupportedGenerators::SystemGenerator,*/ SupportedGenerators::Linear,
+    //                                                    /*SupportedGenerators::X917RNG,*/ /*SupportedGenerators::AESRNG,*/ /*SupportedGenerators::MersenneTwister*/ };
+    //    std::vector<SupportedGenerators> generators = { SupportedGenerators::Knuthan, SupportedGenerators::LFSRSimple };
+    //    std::vector<std::string> inputFileNames = { "inputKnuthan.bin", "inputLFSRSimple.bin" };
+    //    for (size_t i = 0; i < inputFileNames.size(); ++i)
+    //    {
+    //        generateData(inputFileNames[i], generators[i], kLength);
+    //    }
 
-        /*for (size_t i = 0; i < inputFileNames.size(); ++i)
-        {
-            launchAnalysisTestAllSetsAuto<3>(inputFileNames[i], outpuFileNames[i]);
-        }
-    }*/
+    //    /*for (size_t i = 0; i < inputFileNames.size(); ++i)
+    //    {
+    //        launchAnalysisTestAllSetsAuto<3>(inputFileNames[i], outpuFileNames[i]);
+    //    }*/
+    //}
 
-    //launchAnalysisTestAllSetsAuto<3>("1/inputLinear.bin", "1/outputLinearTest.csv");
+    launchAnalysisTestAllSetsAuto<3>("1/inputLinear.bin", "1/outputLinearTest.csv");
 
     //auto result = getRandomBlockFromGenerator(SupportedGenerators::LFSRSimple, 1024);
 
